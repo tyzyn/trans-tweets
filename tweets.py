6a49a60694760eaa5b8b341d00d01d013fdbd7dc
tyzyn <chris-tyson@outlook.com>
2021-08-30 19:16:27 +0100

    Changed output method + added new track word

--- tweets.py
+++ tweets.py
@@ -13,37 +13,18 @@ parser.add_argument("-p", "--password", dest="password")
 
 args = parser.parse_args()
 
-db = mysql.connector.connect(
-    host="localhost",
-    user=args.username,
-    password=args.password,
-    database="tweets"
-)
-
 class MyStreamListener(tweepy.StreamListener):
 
-    def __init__(self, api, db):
+    def __init__(self, api):
         self.api = api
-        self.db = db
-        self.cursor = self.db.cursor()
 
     def on_status(self, tweet):
         j = tweet._json
-
-        if j["truncated"]:
-            text = j["extended_tweet"]["full_text"]
-        else:
-            text = j["text"]
-
-        text = text.encode("ascii", "ignore").decode()
-        user = j["user"]["screen_name"]
-
-        print(user)
-        print(len(text))
-        sql = "INSERT INTO trans (time, user, text) VALUES (%s, %s, %s)"
-        val = (datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'), user, text)
-        self.cursor.execute(sql, val)
-        self.db.commit()
+        created_at = datetime.strftime(datetime.strptime(j['created_at'], '%a %b %d %H:%M:%S +0000 %Y'), '%Y-%m-%d %H:%M:%S')
+        dt = created_at.split()[0].replace('-', '') + created_at.split()[1].split(':')[0]
+        with open("data/"+args.track.replace(".txt","")+"_"+dt+".jsonl", "a+") as fout:
+            fout.write(json.dumps(j)+"\n")
+        print("Tweet by", j['user']['screen_name'], "at", created_at)
 
 #grab credentials from txt file
 with open("creds/"+args.credentials, "r") as fin:
@@ -59,6 +40,6 @@ with open("track/"+args.track, "r") as fin:
     track = [line.strip() for line in fin.readlines()]
 
 print(track)
-stream = MyStreamListener(api, db)
+stream = MyStreamListener(api)
 stream = tweepy.Stream(api.auth, stream)
 stream.filter(track=track, languages=["en"])
